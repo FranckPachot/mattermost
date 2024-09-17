243a31f72c9bbe79722831f0aec7b5d6e505284f
Franck Pachot (YB) <mail@pachot.net>
2024-09-17 08:14:55 +0000

    Make changes compatible with PostgreSQL and YugabyteDB

--- server/channels/db/migrations/postgres/000111_update_vacuuming.down.sql
+++ server/channels/db/migrations/postgres/000111_update_vacuuming.down.sql
@@ -1,4 +1,14 @@
-alter table posts set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_analyze_scale_factor = 0.1);
-alter table threadmemberships set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_analyze_scale_factor = 0.1);
-alter table fileinfo set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_analyze_scale_factor = 0.1);
-alter table preferences set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_analyze_scale_factor = 0.1);
+declare
+ version text;
+begin
+ select version() into version;
+ -- YugabyteDB has no vaccuum (and shows -YB- in the version string)
+ if version !~ '-YB-' then
+ execute $DDL$
+  alter table posts set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_analyze_scale_factor = 0.1);
+  alter table threadmemberships set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_analyze_scale_factor = 0.1);
+  alter table fileinfo set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_analyze_scale_factor = 0.1);
+  alter table preferences set (autovacuum_vacuum_scale_factor = 0.2, autovacuum_analyze_scale_factor = 0.1);  
+ $DDL$;
+ end if;
+end;
--- server/channels/db/migrations/postgres/000111_update_vacuuming.up.sql
+++ server/channels/db/migrations/postgres/000111_update_vacuuming.up.sql
@@ -1,4 +1,14 @@
-alter table posts set (autovacuum_vacuum_scale_factor = 0.1, autovacuum_analyze_scale_factor = 0.05);
-alter table threadmemberships set (autovacuum_vacuum_scale_factor = 0.1, autovacuum_analyze_scale_factor = 0.05);
-alter table fileinfo set (autovacuum_vacuum_scale_factor = 0.1, autovacuum_analyze_scale_factor = 0.05);
-alter table preferences set (autovacuum_vacuum_scale_factor = 0.1, autovacuum_analyze_scale_factor = 0.05);
+declare
+ version text;
+begin
+ select version() into version;
+ -- YugabyteDB has no vaccuum
+ if version !~ '-YB-' then
+ execute $DDL$
+  alter table posts set (autovacuum_vacuum_scale_factor = 0.1, autovacuum_analyze_scale_factor = 0.05);
+  alter table threadmemberships set (autovacuum_vacuum_scale_factor = 0.1, autovacuum_analyze_scale_factor = 0.05);
+  alter table fileinfo set (autovacuum_vacuum_scale_factor = 0.1, autovacuum_analyze_scale_factor = 0.05);
+  alter table preferences set (autovacuum_vacuum_scale_factor = 0.1, autovacuum_analyze_scale_factor = 0.05);
+ $DDL$;
+ end if;
+end;
